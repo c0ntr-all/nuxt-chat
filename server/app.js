d805e97b182d84a5aebefbfa9a3d609d8dff2e08
unrealserg <serg.taturin@gmail.com>
2021-08-04 00:18:52 +0300

    Server side configured

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const { Socket } = require('socket.io')
+
+const app = require('express')()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+io.on('connection', socket => {
+    console.log('IO connected')
+})
+
+module.exports = {
+    app,
+    server
+}
