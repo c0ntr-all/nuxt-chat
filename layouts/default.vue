3431e90266910fd85c56d11dd7b28d67f2791b9f
unrealserg <serg.taturin@gmail.com>
2021-08-05 21:00:12 +0300

    Added basic layout for users list in chat room

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,23 @@
 <template>
   <v-app app dark>
-    <v-navigation-drawer app v-model="drawer"></v-navigation-drawer>
+    <v-navigation-drawer app v-model="drawer">
+      <v-list subheader>
+        <v-subheader>Recent chat</v-subheader>
+        <v-list-item v-for="chat in recent" :key="chat.title">
+          <v-list-item-avatar>
+            <v-img :alt="`${chat.title} avatar`" :src="chat.avatar"></v-img>
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title v-text="chat.title"></v-list-item-title>
+          </v-list-item-content>
+          <v-list-item-icon>
+            <v-icon :color="chat.active ? 'deep-purple accent-4' : 'grey'">
+              mdi-message-outline
+            </v-icon>
+          </v-list-item-icon>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-btn icon @click="exit">
