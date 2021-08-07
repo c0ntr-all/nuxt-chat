e18f7eee582f4b77d10a9601b70211dcd4fa494c
unrealserg <serg.taturin@gmail.com>
2021-08-08 01:58:47 +0300

    Added autoscroll to new messages

--- pages/chat.vue
+++ pages/chat.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="c-wrap">
-    <div class="c-chat">
+    <div class="c-chat" ref="block">
       <Message v-for="m in messages" :key="m.text" :name="m.name" :text="m.text" :owner="m.id === user.id"/>
     </div>
     <div class="c-form">
@@ -21,7 +21,14 @@ export default {
     }
   },
   components: { Message, ChatForm },
-  computed: mapState(['user', 'messages'])
+  computed: mapState(['user', 'messages']),
+  watch: {
+    messages() {
+      setTimeout(()) => {
+        this.$refs.block.scrollTop = this.$refs.block.scrollHeight
+      }
+    }
+  }
 };
 </script>
 
