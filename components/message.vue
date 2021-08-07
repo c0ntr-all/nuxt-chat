85325af11f0d1eff5bedf2e9e7ad4a9b3e814818
unrealserg <serg.taturin@gmail.com>
2021-08-07 23:50:31 +0300

    Base template for messages

---
+++ components/message.vue
@@ -0,0 +1,28 @@
+<template>
+    <div v-if="name === 'admin'" class="system">
+        <p class="text-xs-center">{{text}}</p>
+        <hr>
+    </div>
+    <div v-else class="wrap">
+        <div class="mes" :class="{owner}">
+            <small><strong>{{name}}</strong></small>
+            <p>{{text}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            name: String,
+            text: String,
+            owner: {
+                type: Boolean,
+                default: false
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+</style>
