05ec3f3573e60875e3b379ccf4d01aef07110a70
unrealserg <serg.taturin@gmail.com>
2021-08-08 00:46:43 +0300

    Corrected styles for messages

--- components/message.vue
+++ components/message.vue
@@ -27,10 +27,9 @@
 <style scoped>
 .system {
   margin-bottom: 1rem;
-
-  p {
-    margin-bottom: 1rem;
-  }
+}
+.system p {
+  margin-bottom: 1rem;
 }
 .wrap {
   display: flex;
@@ -40,14 +39,14 @@
   position: relative;
   width: 60%;
   padding: 1rem 1rem;
-  margin: 0 1rem;
+  margin: 0 1rem 1rem;
   box-shadow: 0 1px 0 0 rgba(50, 50, 50, 0.3);
   border-radius: 4px;
   background: #1976d2;
+}
 
-  p {
-    margin-bottom: 0;
-  }
+.mes p {
+  margin-bottom: 0;
 }
 
 .owner {
